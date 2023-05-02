79076616989d88f82f15362439894f24df9d5e8b
Abhishek Bhattacharjee <abhishek.bhattacharjee.job@gmail.com>
2023-05-02 17:58:29 +0530

    adding server.py

---
+++ server.py
@@ -0,0 +1,29 @@
+import json
+from flask import Flask, request, Response, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/only_response')
+def only_response():
+    response = jsonify({"response":"This is a dummy response"})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/request_response', methods=['POST'])
+def request_response(request=request):
+    req_data = request.get_json()
+    response = jsonify({"response":"This is a dummy response", "yourRequestData":req_data["request_data"]})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+    #return Response(json.dumps({"response":query}), status=200, mimetype='application/json') 
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port="3001")
+
+
+
+
+
+
+#
